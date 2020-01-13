ea3e613232653ba1d2c03af4798022bda732acd9
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-01-13 20:47:51 -0300

    First commi

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿function adicionarDiv() {
+    var divElement = document.createElement('div');
+    var text = document.createTextNode("TESTE");
+    var body = document.body;
+
+    divElement.style.height = 100;
+    divElement.style.width = 100;
+    divElement.style.backgroundColor = 'RED';
+
+    body.appendChild(divElement);
+}
+
+var btnAdicionarDiv = document.getElementById("btnAdicionarDiv");
+btnAdicionarDiv.onclick = adicionarDiv;
